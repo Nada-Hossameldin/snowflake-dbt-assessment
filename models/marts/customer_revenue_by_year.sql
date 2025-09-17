afd53709f6aa47886a1145a2d9990d11beb72260
Nada-Hossameldin <nadaalgheriany@gmail.com>
2025-09-18 02:40:25 +0300

    Add dbt project for Snowflake TPCH assessment

--- models/marts/customer_revenue_by_year.sql
+++ models/marts/customer_revenue_by_year.sql
@@ -4,22 +4,24 @@ with order_revenue as (
     select
         o.o_orderkey,
         o.o_custkey,
+        year(o.o_orderdate) as order_year,
         sum(l.l_extendedprice * (1 - l.l_discount)) as revenue
     from {{ source('tpch', 'orders') }} o
     join {{ source('tpch', 'lineitem') }} l
       on o.o_orderkey = l.l_orderkey
-    group by o.o_orderkey, o.o_custkey
+    group by o.o_orderkey, o.o_custkey, year(o.o_orderdate)
 ),
 
 customer_revenue as (
     select
         c.c_custkey as customer_id,
         c.c_name as customer_name,
+        orv.order_year,
         sum(orv.revenue) as total_revenue
     from order_revenue orv
     join {{ source('tpch', 'customer') }} c
       on orv.o_custkey = c.c_custkey
-    group by c.c_custkey, c.c_name
+    group by c.c_custkey, c.c_name, orv.order_year
 )
 
 select *
--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,29 +1,17 @@
 {{ config(materialized='view') }}
 
-
-with raw_orders as (
-select
-o_orderkey,
-o_custkey,
-o_orderstatus,
-o_totalprice,
-o_orderdate,
-o_orderpriority,
-o_clerk,
-o_shippriority,
-o_comment
-from {{ source('tpch_sf1', 'orders') }}
-),
-customers as (
-select c_custkey, c_name from {{ source('tpch_sf1', 'customer') }}
+with orders as (
+    select
+        o.o_orderkey,
+        o.o_custkey,
+        o.o_orderdate,
+        year(o.o_orderdate) as order_year,
+        o.o_totalprice as total_price,
+        c.c_name as customer_name
+    from {{ source('tpch', 'orders') }} o
+    join {{ source('tpch', 'customer') }} c
+      on o.o_custkey = c.c_custkey
 )
 
-
-select
-ro.*,
-c.c_name as customer_name,
-extract(year from ro.o_orderdate) as order_year,
-ro.o_totalprice as total_price
-from raw_orders ro
-left join customers c
-on ro.o_custkey = c.c_custkey
+select *
+from orders
